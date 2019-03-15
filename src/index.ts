1f19bcd39ec00ccb04c8aa9d6b740bbacd24e1f7
ihlee <ihlee@jlk-inspection.com>
2019-03-15 17:52:44 +0900

    creating block wip

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
+import * as CryptoJS from "crypto-js";
+
 class Block{
     public index:number;
     public hash: string;
     public prevHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (
+        index:number,
+        prevHash:string,
+        timestamp:number,
+        data:string
+        ):string => CryptoJS.SHA256(index + prevHash + timestamp + data).toString
+
+    static validateStructure = (aBlock:Block) : boolean =>
+        typeof aBlock.index === "number" &&
+        typeof aBlock.hash == "string" &&
+        typeof aBlock.prevHash === "string" &&
+        typeof aBlock.timestamp === "number" &&
+        typeof aBlock.data === "string";
+
     constructor(
         index:number,
         hash: string,
@@ -19,8 +36,36 @@ class Block{
     }
 }
 
-const genesisBlock:Block = new Block(0, "2020202020", "", "Hello", 123456);
+Block.calculateBlockHash
+
+const genesisBlock:Block = new Block(0, "aaaaaa", "", "Hello", 123456);
 
 let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain)
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data:string) : Block => {
+    const prevBlock: Block = getLatestBlock();
+    const newIndex : number = prevBlock.index + 1;
+    const newTimestamp : number = getNewTimeStamp();
+    const newHash : string = Block.calculateBlockHash(newIndex, prevBlock.hash, newTimestamp, data);
+    const newBlock : Block = new Block(newIndex, newHash, prevBlock.hash, data, newTimestamp);
+    return newBlock;
+};
+
+const isBlockValid = (candidateBlock : Block, prevBlock : Block) : boolean => {
+    if(Block.validateStructure(candidateBlock)) {
+        return false;
+    } else if (prevBlock.index + 1 !== candidateBlock.index){
+        return false;
+    } else if (prevBlock.hash !== candidateBlock.prevHash){
+        return false;
+    }
+}
+
+console.log('createNewBlock("hello") :', createNewBlock("hello"));
+console.log('createNewBlock("bye bye") :', createNewBlock("bye bye"));
